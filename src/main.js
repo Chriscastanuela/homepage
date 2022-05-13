55ccd0f593096b673129e6833b58e4aabdccd234
Chriscastanuela <chriscastanuela@ymail.com>
2022-05-12 23:15:17 -0600

    add menu functionality

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
+const xButton = document.getElementById('home__nav--button-x');
+
 const homeSection = document.querySelector('.home');
 const projectsSection = document.querySelector('.projects');
+const menu = document.querySelector('.menu');
+
+let visibility = false;
+
+const theHiddenThing = (itemInQuestion) => {
+    if (visibility === false) {
+        itemInQuestion.style.visibility = 'visible';
+        visibility = true;
+    } else {
+        itemInQuestion.style.visibility = 'hidden';
+        visibility = false;
+    }
+
+};
+const openAndClose = () => {
+    xButton.classList.toggle('home__nav--button-x-open');
+    theHiddenThing(menu);
+}
+
+xButton.onclick = () => {openAndClose()};
